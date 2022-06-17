eb80052b80637c5721ac36cd2ee00c90064c117b
raj shringi <raj24shringi@gmail.com>
2022-06-17 13:38:41 +0530

    complete all todos

---
+++ block-BNaacn/sample/models/article.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: String,
+  description: String,
+  tags: [String],
+  createdAt: new Date(),
+  likes: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
